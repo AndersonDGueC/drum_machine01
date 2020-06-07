c8d3954d8b5bc5189359964ef6f7ff85997d4769
anderson <adguerreroc@unal.edu.co>
2020-06-07 10:48:54 -0500

    Updates

---
+++ precache-manifest.2b67691070ceae0b06e69a2c3525a069.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac2fac0dd236b9b9a49bef0316a30c1a",
+    "url": "/drum_machine01/index.html"
+  },
+  {
+    "revision": "3c219954420227ae7e9b",
+    "url": "/drum_machine01/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4e4098fed40dc5f215f9",
+    "url": "/drum_machine01/static/css/main.3b7ed0fc.chunk.css"
+  },
+  {
+    "revision": "3c219954420227ae7e9b",
+    "url": "/drum_machine01/static/js/2.dd9b4a76.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drum_machine01/static/js/2.dd9b4a76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e4098fed40dc5f215f9",
+    "url": "/drum_machine01/static/js/main.ae5f7485.chunk.js"
+  },
+  {
+    "revision": "91df5a2ec83736f8b5c8",
+    "url": "/drum_machine01/static/js/runtime-main.f3a23239.js"
+  }
+]);
